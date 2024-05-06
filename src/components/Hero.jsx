b5dd8f938ab8c3d1cd44d2afd80fa1c0f7b6ad8f
Mridulians <gmridul898@gmail.com>
2024-05-06 17:23:40 +0530

    production ready 2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <div className="hero">
       <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between dark:text-white">
         <div className="left flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
-          <h1 className="text-5xl font-bold leading-none sm:text-6xl mt-6 mb-8">
+          <h1 className="text-5xl text-white font-bold leading-none sm:text-6xl mt-6 mb-8">
             Mining{" "}
             <span className="bg-gradient-to-r from-fuchsia-600 to-purple-600 bg-clip-text text-transparent">
               Crypto{" "}
